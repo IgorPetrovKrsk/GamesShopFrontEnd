3e0f4deb0490cde79062af6a2b476d4401d08b8f
IgorPetrovKrsk <igor.petrov.krsk@gmail.com>
2025-05-29 14:02:15 -0600

    Created UserContext

--- src/context/appContext.jsx
+++ src/context/appContext.jsx
@@ -1,5 +1,9 @@
 import AuthProvider from "./auth/authContext";
+import UserProvider from "./user/userContext";
 
-export default function AppProvider({children}){
-    return <AuthProvider>{children}</AuthProvider>
+export default function AppProvider({ children }) {
+    return <AuthProvider>
+        <UserProvider>{children}</UserProvider>
+    </AuthProvider>
+
 }
